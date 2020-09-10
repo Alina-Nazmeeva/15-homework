f2a58eec639f4c07a11db8695ed0298fbd50a82c
Alina Nazmeeva <ell122zmey@gmail.com>
2020-09-11 00:52:29 +0300

    Updates

---
+++ precache-manifest.6e0cb3899cf2d673583d739069d8296b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8528449518b356e1727e54c3cba4252d",
+    "url": "/15-homework/index.html"
+  },
+  {
+    "revision": "bda898619e0c8fb6b60f",
+    "url": "/15-homework/static/css/main.fb468fd6.chunk.css"
+  },
+  {
+    "revision": "bb4ae4b0cade9381c829",
+    "url": "/15-homework/static/js/2.e7e5044d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/15-homework/static/js/2.e7e5044d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bda898619e0c8fb6b60f",
+    "url": "/15-homework/static/js/main.7a7cc952.chunk.js"
+  },
+  {
+    "revision": "5f389a672654d4feea44",
+    "url": "/15-homework/static/js/runtime-main.30443193.js"
+  }
+]);
